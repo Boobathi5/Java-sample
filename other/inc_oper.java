57cb2f8ec5ff767fb613dff7236505a7df2e77eb
Boobathi5 <boobathiraja776@gmail.com>
2024-11-12 21:23:52 +0530

    java example programs

---
+++ other/inc_oper.java
@@ -0,0 +1,22 @@
+class inc_oper
+{
+    public static void main(String args[])
+    {
+        int i=10,j=5;
+        int k;
+        another obj=new another();
+        k=obj.max(++i,j++);
+      
+
+    }
+
+    class another
+    {
+         public  int max(int x,int y)
+        {
+            int re;
+            re=
+            return re;
+        }
+    }
+}
